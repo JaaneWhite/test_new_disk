5d16f209f032f01f7bdb6aa9269e7b16b3c413dc
Anastasiya Shironosova <a.shironosova@altsoft.spb.ru>
2024-11-18 20:44:46 +0300

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  publicPath: '/jaanewhite.github.io/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
